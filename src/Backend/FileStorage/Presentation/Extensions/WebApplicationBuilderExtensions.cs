767b035f6dc278eb9d6083f040cc2e6dc9c00cc0
Abdussattar Kassymbekov <a.kassymbekov@velvetech.dev>
2025-07-14 22:53:51 +0600

    added more filters for logging

--- src/Backend/FileStorage/Presentation/Extensions/WebApplicationBuilderExtensions.cs
+++ src/Backend/FileStorage/Presentation/Extensions/WebApplicationBuilderExtensions.cs
@@ -102,6 +102,9 @@ public static class WebApplicationBuilderExtensions
             .AddFilter("Microsoft.AspNetCore.HttpLogging", LogLevel.Information)
             .AddFilter("Microsoft.AspNetCore.Hosting", LogLevel.Information)
             .AddFilter("Microsoft.AspNetCore.Routing", LogLevel.Information)
+            .AddFilter("Microsoft.AspNetCore.Authentication", LogLevel.Information)
+            .AddFilter("Microsoft.AspNetCore.Diagnostics", LogLevel.Information)
+            .AddFilter("Microsoft.AspNetCore.Analyzers", LogLevel.Information)
             .AddZLoggerConsole(options =>
             {
                 options.TimeProvider = TimeProvider.System;
